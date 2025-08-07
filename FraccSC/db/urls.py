aa2a62f847f7716a6cbf474b39f66f413144a3cb
Diego Audiffred <d1360.audi@gmail.com>
2025-08-07 16:34:33 -0600

    sadasd

---
+++ FraccSC/db/urls.py
@@ -0,0 +1,5 @@
+# myapp/urls.py
+from django.urls import path
+from . import views
+app_name='db'
+
---
+++ FraccSC/index/urls.py
@@ -0,0 +1,9 @@
+# myapp/urls.py
+from django.urls import path
+from . import views
+app_name='index'
+
+urlpatterns = [
+        path("", views.index, name='index'),
+
+]
